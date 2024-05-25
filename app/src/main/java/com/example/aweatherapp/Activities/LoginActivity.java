1e939052d1cccef1d1f18f155e96e56e1ee1a83c
Hannah <87335534+AngelHannah@users.noreply.github.com>
2024-05-24 19:56:38 -0700

    Finished login activity
    Completed code for the login activity

--- app/src/main/java/com/example/aweatherapp/Activities/LoginActivity.java
+++ app/src/main/java/com/example/aweatherapp/Activities/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.aweatherapp.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,8 +26,45 @@ public class  LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //Get the login binding
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        //get the root to display the login and its view
+        //get the root to display the login
         setContentView(binding.getRoot());
+
+        dbHelper = new DatabaseHelper(this);
+
+        //login button
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = binding.loginEmail.getText().toString();
+                String password = binding.loginPassword.getText().toString();
+
+                if(email.equals("") || password.equals("")){
+                    Toast.makeText(LoginActivity.this, "Email and password cannot be blank", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Boolean checkCreds = dbHelper.checkEmailAndPassword(email, password);
+
+                    if(checkCreds == true){
+                        Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                       Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                       startActivity(intent);
+                    }
+                    else{
+                        Toast.makeText(LoginActivity.this, "Email or password did not match, please retype them and try again", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        //for the link to sign up
+        binding.signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
